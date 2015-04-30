1aae192e079d58c4f9a3ced060f1cd390066cb10
Rey <rmmedillo@yahoo.com.ph>
2015-04-30 17:06:36 +0800

    04302015 5.06

--- app/Controller/InquiryController.php
+++ app/Controller/InquiryController.php
@@ -7,12 +7,39 @@ class InquiryController extends AppController
 	public $helpers = array('Html', 'Form');
 	public $components = array('RequestHandler');
 	public $uses = array('Inquiry');
+	public $scaffold;
 	
-public function index() {
-	$this->autoRender = false;
-	$inq = $this->Inquiry->find('all');
-	echo json_encode($inq);
-}
+	function index() {
+		$this->autoRender = false;
+		$inq = $this->Inquiry->find('all');
+        $this->set(array(
+            'inq' => $inq,
+            '_serialize' => array('inq'),
+			'_jsonp' => true
+        ));
+	}
+	
+	function add() {
+		$this->autoRender = false;
+		if($this->request->is('post')) {
+			if ($this->Inquiry->save($this->request->data)) {
+	            $inq = array(
+	                'text' => __('Saved'),
+	                'type' => 'success'
+	            );
+	        } else {
+	            $inq = array(
+	                'text' => __('Error'),
+	                'type' => 'error'
+	            );
+	        }
+	        $this->set(array(
+	            'inq' => $inq,
+	            '_serialize' => array('inq'),
+			'_jsonp' => true
+	        ));
+		}
+	}
 
 }
 
